c8b84bb62659be7c9d07531d922f88d1f5885877
Pete OConnell <pete-oconnell@users.noreply.github.com>
2017-09-05 22:14:28 +0100

    Add items to table and alert with row data when pushed

---
+++ tableApp/TableSource.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Foundation;
+using UIKit;
+
+namespace tableApp
+{
+    public class TableSource : UITableViewSource
+    {
+        string[] tableItems;
+        string CellIdentifier = "TableCell";
+        ViewController owner;
+
+        public TableSource(string[] items, ViewController owner)
+        {
+            tableItems = items;
+            this.owner = owner;
+        }
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+			return tableItems.Length;
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);
+			string item = tableItems[indexPath.Row];
+
+			//---- if there are no cells to reuse, create a new one
+			if (cell == null)
+			{ cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier); }
+
+			cell.TextLabel.Text = item;
+
+			return cell;
+		}
+
+		public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+		{
+			UIAlertController okAlertController = UIAlertController.Create("Row Selected", tableItems[indexPath.Row], UIAlertControllerStyle.Alert);
+			okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+            owner.PresentViewController(okAlertController,true,null);
+            tableView.DeselectRow(indexPath, true);
+		}
+    }
+}
